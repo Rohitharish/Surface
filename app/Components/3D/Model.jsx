88a7bac354a547069dde41de6cd00ea6b28c398a
Rohitharish <rohitharish276@gmail.com>
2024-11-15 17:40:40 +0530

    trail3

--- app/Components/3D/Model.jsx
+++ app/Components/3D/Model.jsx
@@ -3,9 +3,9 @@ import React, { useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 
 export default function Model() {
-  const { scene } = useGLTF('/sphere.glb');
+  const { scene } = useGLTF('/Model.glb');
 
   return <primitive object={scene} />;
 }
 
-useGLTF.preload('/sphere.glb');
+useGLTF.preload('/Model.glb');
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Rewrite rules to map the URL to the public directory
   async rewrites() {
     return [
       {
@@ -7,6 +8,24 @@ const nextConfig = {
         destination: '/public/Model.glb', // Ensure your Model is in the public directory
       },
     ];
+  },
+
+  // Custom Webpack configuration to handle .glb files using url-loader
+  webpack(config) {
+    // Adding a rule to handle .glb files
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // You can adjust this limit based on the file size
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
 
